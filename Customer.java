da4b81b729f04f7175cf83fd159f1fee410c8a13
Justin <justinschwerdtfeger245@gmail.com>
2026-03-23 12:57:04 -0500

    Add getters to Customer

--- Customer.java
+++ Customer.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Customer {
     private final String _name;
     private final ArrayList<Rental> _rentals = new ArrayList<>();
-    private double totalAmount          = 0;
-    private int    totalFrequentRenterPoints = 0;
+    private double _totalAmount          = 0;
+    private int    _totalFrequentRenterPoints = 0;
     
     public Customer(String name) {
         _name = name;
@@ -12,16 +12,24 @@ public class Customer {
     
     public void addRental(Rental arg) {
         _rentals.add(arg);
-        totalAmount += arg.computeRentalPrice();
-        totalFrequentRenterPoints += arg.computeFrequentRentalPoints();
+        _totalAmount += arg.computeRentalPrice();
+        _totalFrequentRenterPoints += arg.computeFrequentRentalPoints();
     }
     
     public String getName() {
         return _name;
     }
 
+    public ArrayList<Rental> getRentals() {
+        return _rentals;
+    }
+
+    public double getTotalAmount() {
+        return _totalAmount;
+    }
+
     public int getTotalFrequentRenterPoints() {
-        return totalFrequentRenterPoints;
+        return _totalFrequentRenterPoints;
     }
     
     public String generateStatement() {
@@ -38,8 +46,8 @@ public class Customer {
         }
         
         // add footer lines
-        result.append("Amount owed is ").append(totalAmount).append("\n");
-        result.append("You earned ").append(totalFrequentRenterPoints).append(" frequent renter points");
+        result.append("Amount owed is ").append(_totalAmount).append("\n");
+        result.append("You earned ").append(_totalFrequentRenterPoints).append(" frequent renter points");
         return result.toString();
     }
 
@@ -54,8 +62,8 @@ public class Customer {
             result.append("\t</rental>\n");
         }
         
-        result.append("\t<amountOwed> ").append(totalAmount).append(" </amountOwed>\n");
-        result.append("\t<frequentRenterPoints> ").append(totalFrequentRenterPoints).append(" </frequentRenterPoints>\n");
+        result.append("\t<amountOwed> ").append(_totalAmount).append(" </amountOwed>\n");
+        result.append("\t<frequentRenterPoints> ").append(_totalFrequentRenterPoints).append(" </frequentRenterPoints>\n");
         result.append("</statement>\n");
         return result.toString();
     }
