759a0d6bb72b7db5ad14b69b5c90a9b9b4b182af
Justin <justinschwerdtfeger245@gmail.com>
2026-03-23 12:21:47 -0500

    Add more to testing

--- Main.java
+++ Main.java
@@ -3,7 +3,12 @@ public class Main {
         Customer c = new Customer("Joe Doe");
         c.addRental(new ChildrensRental("Finding Nemo", 4));
         c.addRental(new NewReleaseRental("Iron Lung", 2));
+        c.addRental(new RegularRental("Independence Day", 3));
+
+        System.out.println("---Regular Statement---");
         System.out.println(c.generateStatement());
+        System.out.println();
+        System.out.println("---XML Statement---");
         System.out.println(c.generateXMLStatement());
     }
 }
